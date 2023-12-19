3c46083cd88f85fbd359e468416938d20dcd8da1
Sarthak Khandare <sarthakkhandare21@gmail.com>
2023-12-19 10:36:28 +0530

    main menu

--- chatbot_app.py
+++ chatbot_app.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 
 VERIFY_TOKEN = 'KMFAMILY'
 
-PAGE_ACCESS_TOKEN = 'EAAJztZAX6JRwBO1LkB3ZCSQ5DWfQLUvvUxTRWbSLxf9sPmVoIE4LbsZBZBiTaVsGb6Il05p2X4aOAPa1GqGE44uYJhe6sYRCFXbXxkXs1ScUNxXBz8zU4l74iBjr783VXI3qVdZCne6rJ3n45zReFjAyZAILbXDZBIA3UZCbyolXXiq9kTG4O9BiPwhUGeqg3ugTbtxvZAhQ1h8lqTHjiAj9uptXZCHUyoGVTkPxQZD'
+PAGE_ACCESS_TOKEN = 'EAAJztZAX6JRwBO5eNpXZCtPAJTYvNl2IxxDshgLkHn6sVUvyoDhIdDMr8M6x7OwvvaKKsfTVxmDUOVZC4GiS8PRx0woUyaZAsBZATXJGo0UiMuWPQ1pfZAgITM25xG1ZBp6hX6SWiezvufKMIfK2UIU0e838o8FHWznZC8CuyAxtTorg4OxQh3FKDRwcMaaGwvj9ZBCqCMLbeqUhOttZB2d9IGrUGdLkOi8KUVoFAZD'
 user_preferences = {}
+
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 messages = {
     'en': {
@@ -22,7 +23,29 @@ messages = {
         'documents_uploaded_farmer': 'Thank you for uploading your documents. We will get back to you shortly.',
         'documents_uploaded_dealer': 'Thank you for uploading your documents. We will get back to you shortly.',
         'documents_uploaded': 'Thank you for uploading your documents. We will get back to you shortly.',
+        'document_verification': ' Verification sucessfull',
         'document_validation_failed': 'Sorry, we could not validate your documents. Please try again.',
+        'select_hangam': 'Select Hangam: 1. Kharip, 2. Rabii',
+        'hangam_selected_kharip': 'You have selected Kharip hangam.',
+        'hangam_selected_rabii': 'You have selected Rabii hangam.',
+        'Main_menu_farmer': '1. Crop Prediction  \n 2. Suggestion of Government Schemes \n 3. Nearest APMC \n 4. Industry Recommendation',
+        'Main_menu_dealer': '1. Probable Profit \n 2. Industry Recommendation',
+
+
+
+        'enter_values': 'Please enter the following values 1. Nitrogen 2. Phosphorous 3. Potassium 4. Temperature 5. Humidity 6. PH 7. Rainfall',
+
+        'enter_nitrogen': 'Enter the nitrogen value',
+        'enter_phosphorous': 'Enter the phosphorous value',
+        'enter_potassium': 'Enter the potassium value',
+        'enter_temperature': 'Enter the temperature value',
+        'enter_humidity': 'Enter the humidity value',
+        'enter_ph': 'Enter the ph value',
+        'enter_rainfall': 'Enter the rainfall value',
+
+
+
+
     },
     'hi': {
         'welcome': 'स्वागत है! कृपया भाषा का चयन करें: 1. इंग्लिश, 2. हिंदी, 3. मराठी।',
@@ -32,7 +55,23 @@ messages = {
         'upload_documents_dealer': 'कृपया डीलरशिप के लिए अपना लाइसेंस नंबर और आधार विवरण अपलोड करें।',
         'documents_uploaded_farmer': 'अपने दस्तावेज़ अपलोड करने के लिए धन्यवाद। हम जल्द ही आपसे संपर्क करेंगे।',
         'documents_uploaded_dealer': 'अपने दस्तावेज़ अपलोड करने के लिए धन्यवाद। हम जल्द ही आपसे संपर्क करेंगे।',
+        'document_verification': ' apka verification hogaya hai ',
         'documents_uploaded': 'अपने दस्तावेज़ अपलोड करने के लिए धन्यवाद। हम जल्द ही आपसे संपर्क करेंगे।',
+        'select_hangam': 'आप हैंगम का चयन करें: 1. खरीप, 2. रबी',
+        'hangam_selected_kharip': 'आपने खरीप हैंगम का चयन किया है।',
+        'hangam_selected_rabii': 'आपने रबी हैंगम का चयन किया है।',
+        'Main_menu_farmer': '1. फसल की भविष्यवाणी \n 2. सरकारी योजनाओं का सुझाव \n 3. निकटतम एपीएमसी \n 4. उद्योग की सिफारिश',
+        'Main_menu_dealer': '1. संभावित लाभ \n 2. उद्योग की सिफारिश',
+        'enter_values': 'कृपया निम्नलिखित मानों को दर्ज करें 1. नाइट्रोजन 2. फॉस्फोरस 3. पोटैशियम 4. तापमान 5. आर्द्रता 6. पीएच 7. वर्षा',
+
+        'enter_nitrogen': "नाइट्रोजन का मान दर्ज करें",
+        'enter_phosphorous': "फॉस्फोरस का मान दर्ज करें",
+        'enter_potassium': "पोटेशियम का मान दर्ज करें",
+        'enter_temperature': "तापमान का मान दर्ज करें",
+        'enter_humidity': "आर्द्रता का मान दर्ज करें",
+        'enter_ph': "पीएच का मान दर्ज करें",
+        'enter_rainfall': "वर्षा का मान दर्ज करें"
+
     },
     'mr': {
         'welcome': 'स्वागत आहे! कृपया भाषा निवडा: 1. इंग्लिश, 2. हिंदी, 3. मराठी.',
@@ -42,7 +81,21 @@ messages = {
         'upload_documents_dealer': 'कृपया व्यापारीक्षेत्रात आपला लायसेंस नंबर आणि आधार विवरण अपलोड करा।',
         'documents_uploaded_farmer': 'आपले दस्तावेज अपलोड करण्याबद्दल धन्यवाद. आम्ही लवकरच आपल्याशी संपर्क साधू.',
         'documents_uploaded_dealer': 'आपले दस्तावेज अपलोड करण्याबद्दल धन्यवाद. आम्ही लवकरच आपल्याशी संपर्क साधू.',
+        'document_verification': ' aple verifcication zale ahe ',
         'documents_uploaded': 'आपले दस्तावेज अपलोड करण्याबद्दल धन्यवाद. आम्ही लवकरच आपल्याशी संपर्क साधू.',
+        'select_hangam': 'हंगाम निवडा: 1. खरीप, 2. रबी',
+        'hangam_selected_kharip': 'तुमची निवड: खरीप हंगाम.',
+        'hangam_selected_rabii': 'तुमची निवड: रबी हंगाम.',
+        'Main_menu_farmer': '1. पीक अंदाज \n 2. सरकारी योजनांचा सुचवणी \n 3. निकटतम एपीएमसी \n 4. उद्योग सिफारिश',
+        'Main_menu_dealer': '1. संभाव्य फायदा \n 2. उद्योग सिफारिश',
+        'enter_values': 'कृपया खालील मूल्ये प्रविष्ट करा 1. नाइट्रोजन 2. फॉस्फरस 3. पोटॅशियम 4. तापमान 5. आर्द्रता 6. पीएच 7. पावसाचे मूल्य',
+        'enter_nitrogen': "नाइट्रोजनचे मूल्य प्रविष्ट करा",
+        'enter_phosphorous': "फॉस्फरसचे मूल्य प्रविष्ट करा",
+        'enter_potassium': "पोटॅशियमचे मूल्य प्रविष्ट करा",
+        'enter_temperature': "तापमानचे मूल्य प्रविष्ट करा",
+        'enter_humidity': "आर्द्रतेचे मूल्य प्रविष्ट करा",
+        'enter_ph': "पीएचचे मूल्य प्रविष्ट करा",
+        'enter_rainfall': "पावसाचे मूल्य प्रविष्ट करा"
     },
 }
 
@@ -51,11 +104,31 @@ MESSAGE_WELCOME = 'welcome'
 MESSAGE_INVALID_OPTION = 'invalid_option'
 MESSAGE_ROLE_SELECTION = 'role_selection'
 MESSAGE_UPLOAD_DOCUMENTS = 'upload_documents'
+MESSAGE_HANGAM_SELECTION = 'select_hangam'
+MESSAGE_DOCUMENT_VERIFICATION = 'document_verification'
+MESSAGE_MAIN_MENU_FARMER = 'Main_menu_farmer'
+MESSAGE_MAIN_MENU_DEALER = 'Main_menu_dealer'
+
+MESSAGE_ENTER_VALUES = 'enter_values'
+MESSAGE_ENTER_NITROGEN = 'enter_nitrogen'
+MESSAGE_ENTER_PHOSPHOROUS = 'enter_phosphorous'
+MESSAGE_ENTER_POTASSIUM = 'enter_potassium'
+MESSAGE_ENTER_TEMPERATURE = 'enter_temperature'
+MESSAGE_ENTER_HUMIDITY = 'enter_humidity'
+MESSAGE_ENTER_PH = 'enter_ph'
+MESSAGE_ENTER_RAINFALL = 'enter_rainfall'
 
 
 @app.route('/')
 def index():
     return "Hello"
+
+
+def get_wa_id(message_value):
+    contacts = message_value.get('contacts', [])
+    if contacts:
+        return contacts[0].get('wa_id')
+    return None
 
 
 def validate_user(wa_id):
@@ -96,18 +169,21 @@ def handle_language_selection(message_body, wa_id):
         userlang = user_preferences.get(wa_id, {}).get('language_selected')
         print(user_preferences)
         send_msg(MESSAGE_ROLE_SELECTION, wa_id, userlang)
+
     elif message_body == 'hi':
         send_msg(MESSAGE_WELCOME, wa_id, 'en')
 
 
-def genrate_media_url(media_id):
+def genrate_media_url(media_id, wa_id):
     headers = {
         'Authorization': f'Bearer {PAGE_ACCESS_TOKEN}',
     }
-    responce = requests.get(
+    response = requests.get(
         f'https://graph.facebook.com/v13.0/{media_id}?phone_number_id=135179146337629', headers=headers)
-    media_url = responce.json()['url']
+    media_url = response.json()['url']
     get_image(media_url)
+    userlang = user_preferences.get(wa_id, {}).get('language_selected')
+    send_msg(MESSAGE_HANGAM_SELECTION, wa_id, userlang)
     return media_url
 
 
@@ -131,7 +207,7 @@ def get_image(media_url):
 def upload_image(content):
     files = {'photo': open(content, 'rb')}
     response = requests.post(
-        'http://192.168.11.120:60000/upload', files=files)
+        'http://192.168.121.120:60000', files=files)
     if response.status_code == 200:
         # print(response.json())
         public_url = response.json()['public_url']
@@ -146,18 +222,15 @@ def upload_image(content):
 
 def extract_data(pub_url):
     data = {'file_url': pub_url}
-    response = requests.post('http://192.168.11.120:60001/extract', json=data)
+    response = requests.post(
+        'http://172.30.69.161:60001/extract', json=data)
     if response.status_code == 200:
-        save_user_data(response.content)
+        logging.info(response.json)
         return response.json()
     else:
         print(
             f"Failed to extract data. Status code: {response.status_code}, Error: {response.text}")
         return None
-
-
-def save_user_data(data):
-    print(data)
 
 
 def handle_role_selection(message_body, wa_id):
@@ -170,12 +243,76 @@ def handle_role_selection(message_body, wa_id):
         selected_language = user_preferences.get(
             wa_id, {}).get('language_selected', 'en')
 
-        msg_key = MESSAGE_INVALID_OPTION
         if selected_role in ['Farmer', 'Dealer']:
             msg_key = MESSAGE_UPLOAD_DOCUMENTS + f'_{selected_role.lower()}'
+            send_msg(msg_key, wa_id, selected_language)
+        else:
+            handle_hangam_selection(message_body, wa_id)
+
+
+def handle_hangam_selection(message_body, wa_id):
+    if message_body in ['1', '2']:
+        hangam_options = ['kharip', 'rabii']
+        selected_hangam = hangam_options[int(message_body) - 1]
+        user_preferences.setdefault(wa_id, {})
+        user_preferences[wa_id]['hangam_selected'] = selected_hangam
+        selected_language = user_preferences.get(
+            wa_id, {}).get('language_selected', 'en')
+        msg_key = f'hangam_selected_{selected_hangam.lower()}'
         send_msg(msg_key, wa_id, selected_language)
-    else:
-        send_msg(MESSAGE_INVALID_OPTION, wa_id)
+        selected_role = user_preferences.get(wa_id, {}).get('role_selected')
+        logging.info(selected_role)
+
+        if selected_role == 'Farmer':
+            msg_key = MESSAGE_MAIN_MENU_FARMER
+            send_msg(msg_key, wa_id, selected_language)
+            main_menu(message_body, wa_id)
+
+        elif selected_role == 'Dealer':
+            msg_key = MESSAGE_MAIN_MENU_DEALER
+            send_msg(msg_key, wa_id, selected_language)
+
+
+def crop_prdection(wa_id, message_body):
+    logging.info("crop prediction ")
+    print(message_body)
+
+
+def government_schemes():
+
+    logging.info("government schemes")
+
+    return None
+
+
+def nearest_apmc():
+    logging.info("nearest apmc")
+
+    return None
+
+
+def industry_recommendation():
+
+    logging.info("industry recommendation")
+    return None
+
+
+def main_menu(message_body, wa_id):
+
+    if message_body in ['1', '2', '3', '4']:
+        main_menu_options = ['Crop Prediction', 'Suggestion of Government Schemes',
+                             'Nearest APMC', 'Industry Recommendation']
+        user_lang = user_preferences.get(wa_id, {}).get('language_selected')
+
+        print(message_body)
+        selected_main_menu = main_menu_options[int(message_body) - 1]
+        logging.info(selected_main_menu)
+
+        if message_body == '1':
+            crop_prdection(wa_id, message_body)
+
+        elif message_body == '2':
+            government_schemes()
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
@@ -192,17 +329,24 @@ def webhook():
                 changes = res['entry'][0].get('changes', [])
                 if changes:
                     message_value = changes[0].get('value', {})
-                    # print("Message Value:", message_value)
-
                     if 'messages' in message_value:
                         for message in message_value['messages']:
                             if 'image' in message:
                                 media_id = message['image']['id']
                                 contacts = message_value.get('contacts', [])
-                                if media_id is not None and contacts:
-                                    genrate_media_url(media_id)
-                                    send_msg(MESSAGE_UPLOAD_DOCUMENTS,
-                                             contacts[0].get('wa_id'), 'en')
+                                if contacts:
+                                    wa_id = contacts[0].get('wa_id')
+                                    userlang = user_preferences.get(
+                                        wa_id, {}).get('language_selected')
+                                    if media_id is not None:
+                                        print(user_preferences)
+                                        genrate_media_url(media_id, wa_id)
+                                        message_key = MESSAGE_DOCUMENT_VERIFICATION
+                                        send_msg(message_key, wa_id, userlang)
+                                    if not wa_id:
+                                        return jsonify({'status': 'error', 'message': 'wa_id not found'})
+                                    message_body = message['text']['body'].lower(
+                                    )
                             elif 'text' in message:
                                 contacts = message_value.get('contacts', [])
                                 if contacts:
@@ -212,13 +356,17 @@ def webhook():
                                         return jsonify({'status': 'error', 'message': 'wa_id not found'})
                                     message_body = message['text']['body'].lower(
                                     )
-                                    # print("Text Message Body:", message_body)
+                                    print(message_body)
                                     if 'language_selected' not in user_preferences.get(wa_id, {}):
                                         handle_language_selection(
                                             message_body, wa_id)
                                     elif 'role_selected' not in user_preferences.get(wa_id, {}):
                                         handle_role_selection(
                                             message_body, wa_id)
+                                    elif 'hangam_selected' not in user_preferences.get(wa_id, {}):
+                                        handle_hangam_selection(
+                                            message_body, wa_id)
+
                             else:
                                 print("Unsupported message type")
         except Exception as e:
